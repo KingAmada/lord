a2d39dca5c48d91be044e66b99955c678c30f503
KingAmada <sarkiamada@gmail.com>
2023-10-02 01:46:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -61,6 +61,15 @@ let audioContext = new AudioContext();
 let audioQueue = [];
 let startTime = 0;
 let source = null;
+    
+function base64ToArrayBuffer(base64) {
+    const binaryString = window.atob(base64);
+    const bytes = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
 
 // Function to play decoded audio buffers
 function playDecodedAudio() {
