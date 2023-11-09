291c8bd8fb8553f0ac94f7185c3e7625e0ccefcf
KingAmada <sarkiamada@gmail.com>
2023-11-09 07:43:28 -0800

    Update openaiProxy.js

--- api/openaiProxy.js
+++ api/openaiProxy.js
@@ -7,17 +7,41 @@ module.exports = async (req, res) => {
 
   const payload = req.body;
 
-  try {
-    const response = await axios.post('https://api.openai.com/v1/chat/completions', payload, {
-      headers: {
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-        'Content-Type': 'application/json'
-      }
-    });
-    
-    return res.status(200).json(response.data);
+  // Check if the payload contains a "type" key to determine the kind of request
+  if (payload.type === 'chat') {
+    // Handle chat completion request
+    try {
+      const response = await axios.post('https://api.openai.com/v1/chat/completions', payload.data, {
+        headers: {
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+          'Content-Type': 'application/json'
+        }
+      });
 
-  } catch (error) {
-    return res.status(error.response?.status || 500).json(error.response?.data || {});
+      return res.status(200).json(response.data);
+
+    } catch (error) {
+      return res.status(error.response?.status || 500).json(error.response?.data || {});
+    }
+  } else if (payload.type === 'tts') {
+    // Handle TTS request
+    try {
+      const response = await axios.post('https://api.openai.com/v1/audio/speech', payload.data, {
+        headers: {
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+          'Content-Type': 'application/json'
+        },
+        responseType: 'arraybuffer' // This is important for binary responses like audio
+      });
+
+      // Send the audio data as a binary response
+      res.setHeader('Content-Type', 'audio/mpeg');
+      return res.send(response.data);
+
+    } catch (error) {
+      return res.status(error.response?.status || 500).json(error.response?.data || {});
+    }
+  } else {
+    return res.status(400).send("Bad Request: Unknown type");
   }
 };
