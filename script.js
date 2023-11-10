0dbd689e9e59cc3020166d26dcf11279ab3ee99e
KingAmada <sarkiamada@gmail.com>
2023-11-09 23:20:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@
     recognition.onend = () => {
   if (!manuallyStopped && isRecognitionActive) {
       programmaticRestart = true;
-      // isRecognitionActive = false;
+      
           console.log("Recognition ended, attempting to restart.");
     // Only restart the recognition if not manually stopped and TTS is not active
     startRecognition();
@@ -180,6 +180,7 @@ isRecognitionActive = false;
          if (!manuallyStopped) {
     startRecognition();
          displayMessage("Listening...", "user");
+             isRecognitionActive = false;
          }
     };
   } catch (error) {
