ceca859c030d53f04a0a1bce15e0505b55c588ca
KingAmada <sarkiamada@gmail.com>
2023-11-10 16:09:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 (function() {
-    // Initialization
-    let recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+    // Speech recognition setup
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    let recognition;
+    if (SpeechRecognition) {
+        recognition = new SpeechRecognition();
+    } else {
+        console.error("Speech recognition not supported in this browser.");
+        return;
+    }
+
     let manuallyStopped = false;
     const RECOGNITION_TIMEOUT = 1000;  
     const INACTIVITY_DURATION = 90000; 
@@ -8,54 +16,44 @@
     let inactivityTimeout;
     let isRecognitionActive = false;
     let programmaticRestart = false;
-    let track = false;
-
     const WAKE_UP_PHRASES = ["Hi"];
-    let conversationHistory = [{
-        role: "system",
-        content: "You are an helpful assistant, but always respond like snoop dog or samuel l jackson or drake or tupac or bob marley or morgan freeman or the weeknd"
-    }];
+    let conversationHistory = [{ role: "system", content: "You are an helpful assistant, but always respond like snoop dog" }];
 
-    // Event Listeners
-    recognition.continuous = false;
-    recognition.lang = 'en-US';
-    recognition.interimResults = false;
-    recognition.maxAlternatives = 1;
-    recognition.onresult = handleRecognitionResult;
-    recognition.onaudiostart = () => { 
-        console.log("Audio capturing started");
-        if (!track) {
-            displayMessage("Listening...", "user");
-            track =false;
-        }
-        programmaticRestart = false;
-    };
-    recognition.onsoundstart = () => { console.log("Some sound is being received"); };
-    recognition.onspeechstart = () => { console.log("Speech has been detected"); };
-    recognition.onstart = () => { 
-    setVoiceButtonState("STOP");
-    setActiveMode();};
-    recognition.onend = () => {
-  if (!manuallyStopped) {
-      programmaticRestart = true;
-          console.log("Recognition ended, attempting to restart.");
-    startRecognition();
-  } 
-    };
-
-
-    function startsWithWakeUpPhrase(message) {
-        return WAKE_UP_PHRASES.some(phrase => message.toLowerCase().startsWith(phrase));
-    }
-
-    if (window.AudioContext && new AudioContext().state === "suspended") {
+    // Initialize audio context
+    let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    if (audioContext.state === "suspended") {
         console.log("Auto-play might be restricted. Need user gesture to enable.");
-        document.addEventListener('click', function resumeAudioContext(event) {
-            new AudioContext().resume().then(() => {
+        document.addEventListener('click', function resumeAudioContext() {
+            audioContext.resume().then(() => {
                 console.log("AudioContext resumed successfully");
                 document.removeEventListener('click', resumeAudioContext);
             });
         });
+    }
+
+    // Recognition event listeners
+    recognition.continuous = false;
+    recognition.lang = 'en-US';
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+
+    recognition.onresult = handleRecognitionResult;
+    recognition.onaudiostart = () => console.log("Audio capturing started");
+    recognition.onsoundstart = () => console.log("Some sound is being received");
+    recognition.onspeechstart = () => console.log("Speech has been detected");
+    recognition.onstart = () => {
+        setVoiceButtonState("STOP");
+        setActiveMode();
+    };
+    recognition.onend = () => {
+        if (!manuallyStopped) {
+            programmaticRestart = true;
+            startRecognition();
+        }
+    };
+
+    function startsWithWakeUpPhrase(message) {
+        return WAKE_UP_PHRASES.some(phrase => message.toLowerCase().startsWith(phrase));
     }
 
     function handleRecognitionResult(event) {
@@ -75,29 +73,27 @@
             processCommand(userMessage);
         }
     }
-function startRecognition() {
-    // Check if the recognition is already active to prevent double-start errors)
-    if (!isRecognitionActive) {
-        console.log("Recognition ended, attempting to restart second stage.");
-        try {
-            recognition.start();
-            //isRecognitionActive = true;
-            console.log("Recognition started.");
-            setVoiceButtonState("STOP");
-        } catch (e) {
-            // Handle the error, e.g., if the recognition is already started
-            console.error("Error starting recognition:", e);
+
+    function startRecognition() {
+        if (!isRecognitionActive) {
+            try {
+                recognition.start();
+                isRecognitionActive = true;
+                setVoiceButtonState("STOP");
+            } catch (e) {
+                console.error("Error starting recognition:", e);
+            }
         }
     }
-}
 
-function stopRecognition() {
-    // Only attempt to stop if the recognition is currently active
-        recognition.stop();
-        isRecognitionActive = false;
-        console.log("Recognition stopped.");
-        setVoiceButtonState("START");
-}
+    function stopRecognition() {
+        if (isRecognitionActive) {
+            recognition.stop();
+            isRecognitionActive = false;
+            setVoiceButtonState("START");
+        }
+    }
+
     function processCommand(command) {
         getChatCompletion(command).then(displayAndSpeak);
         resetActiveTimer();
@@ -121,12 +117,10 @@ function stopRecognition() {
     }
 
     function resetActiveTimer() {
-        if (isAwakened) {
-            clearTimeout(inactivityTimeout);
-            inactivityTimeout = setTimeout(() => {
-                isAwakened = false;
-            }, INACTIVITY_DURATION);
-        }
+        clearTimeout(inactivityTimeout);
+        inactivityTimeout = setTimeout(() => {
+            isAwakened = false;
+        }, INACTIVITY_DURATION);
     }
 
     function displayMessage(message, role) {
@@ -136,123 +130,99 @@ function stopRecognition() {
         messageItem.textContent = message;
         messageList.appendChild(messageItem);
         messageList.scrollTop = messageList.scrollHeight;
-        return messageItem;
     }
 
-  async function textToSpeech(text) {
-  const endpoint = 'https://lordne.vercel.app/api/openaiProxy';
-  try {
-    const response = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        type: 'tts', // Indicate that this is a TTS request
-        data: { // Data for the TTS request
-          model: "tts-1",
-          voice: "alloy",
-          input: text
+    async function textToSpeech(text) {
+        const endpoint = 'https://lordne.vercel.app/api/openaiProxy';
+        try {
+            const response = await fetch(endpoint, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    type: 'tts',
+                    data: { model: "tts-1", voice: "alloy", input: text }
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            stopRecognition();
+            const audioData = await response.blob();
+            const audioUrl = URL.createObjectURL(audioData);
+            const audio = new Audio(audioUrl);
+            audio.play();
+            audio.onended = () => {
+                if (!manuallyStopped) startRecognition();
+            };
+        } catch (error) {
+            console.error('Error with TTS request:', error);
         }
-      })
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
     }
-
-      stopRecognition();
-      isRecognitionActive = false;
-      setVoiceButtonState("LISTENING");
-    const audioData = await response.blob();
-    // Play the audio blob with an audio element
-    const audioUrl = URL.createObjectURL(audioData);
-    const audio = new Audio(audioUrl);
-    audio.play();
-       // Update the UI to reflect that the assistant has finished speaking
-      audio.onended = () => {
-         if (!manuallyStopped) {
-             track=true;
-    startRecognition();
-           isRecognitionActive = false;
-         }
-    };
-  } catch (error) {
-    console.error('There was an error with the text-to-speech request:', error);
-  }
-}
 
     function setVoiceButtonState(state) {
-         const voiceButton = document.getElementById("voice-btn");
-    if (state === "START") {
-        voiceButton.textContent = "START";
-        voiceButton.classList.remove("active", "listening");
-    } else if (state === "STOP") {
-        voiceButton.textContent = "STOP";
-        voiceButton.classList.add("active");
+        const voiceButton = document.getElementById("voice-btn");
+        if (state === "START") {
+            voiceButton.textContent = "START";
+            voiceButton.classList.remove("active", "listening");
+        } else if (state === "STOP") {
+            voiceButton.textContent = "STOP";
+            voiceButton.classList.add("active");
         } else if (state === "LISTENING") {
             voiceButton.innerHTML = '<img src="https://kingamada.github.io/lord/listeng.gif" alt="Listening...">';
-        voiceButton.classList.add("listening");
+            voiceButton.classList.add("listening");
         }
     }
+
+    // Voice button event listener
     const voiceButton = document.getElementById("voice-btn");
-    voiceButton.addEventListener("click", function() {
+    voiceButton.addEventListener("click", () => {
         if (voiceButton.textContent === "START" || voiceButton.querySelector("svg")) {
-    manuallyStopped = false;
-    startRecognition();
-  } else {
-    manuallyStopped = true;
-    stopRecognition();
+            manuallyStopped = false;
+            startRecognition();
+        } else {
+            manuallyStopped = true;
+            stopRecognition();
             const messageList = document.getElementById("message-list");
             const lastMessage = messageList.lastChild;
             if (lastMessage && lastMessage.textContent === "Listening...") {
                 messageList.removeChild(lastMessage);
             }
-  }
+        }
     });
-   
+
     const MODEL_PRIORITY = ["gpt-4", "gpt-3.5-turbo", "gpt-3", "gpt-2"];
 
-    async function getChatCompletion(prompt, modelIndex = 0) {
-        if (modelIndex >= MODEL_PRIORITY.length) {
-            console.error("All models exhausted.");
-            return "Sorry, I encountered multiple errors. Please try again later.";
-        }
+    async function getChatCompletion(prompt) {
+        for (let modelIndex = 0; modelIndex < MODEL_PRIORITY.length; modelIndex++) {
+            try {
+                const currentModel = MODEL_PRIORITY[modelIndex];
+                conversationHistory.push({ role: "user", content: prompt });
+                const endpoint = "https://lordne.vercel.app/api/openaiProxy";
+                const response = await fetch(endpoint, {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({
+                        type: 'chat',
+                        data: { model: currentModel, messages: conversationHistory }
+                    })
+                });
 
-        const currentModel = MODEL_PRIORITY[modelIndex];
-        conversationHistory.push({ role: "system", content: prompt });
-        const endpoint = "https://lordne.vercel.app/api/openaiProxy";
-        const payload = {
-             type: 'chat', // Indicate that this is a chat request
-    data: { // Data for the chat completion request
-      model: currentModel,
-      messages: conversationHistory
-    }
-        };
+                if (!response.ok) {
+                    throw new Error(`API Error: ${response.statusText}`);
+                }
 
-        try {
-            console.log("Sending request with payload:", JSON.stringify(payload));
-            
-            const response = await fetch(endpoint, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(payload)
-            });
-
-            if (!response.ok) {
-                throw new Error(`API Error: ${response.statusText}`);
+                const jsonResponse = await response.json();
+                const assistantReply = jsonResponse.choices[0].message.content;
+                conversationHistory.push({ role: "assistant", content: assistantReply });
+                return assistantReply;
+            } catch (error) {
+                console.warn(`Error using model ${MODEL_PRIORITY[modelIndex]}. Error: ${error.message}`);
+                if (modelIndex === MODEL_PRIORITY.length - 1) {
+                    return "Sorry, I encountered an error. Please try again later.";
+                }
             }
-
-            const jsonResponse = await response.json();
-            const assistantReply = jsonResponse.choices[0].message.content;
-            conversationHistory.push({ role: "assistant", content: assistantReply });
-            return assistantReply;
-
-        } catch (error) {
-            console.warn(`Error using model ${currentModel}. Trying next model. Error: ${error.message}`);
-            return await getChatCompletion(prompt, modelIndex + 1);
         }
     }
 })();
